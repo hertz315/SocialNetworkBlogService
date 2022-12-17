0912423cff3974b2c58ac77145ff8a83ce5d5048
hertz315 <hertz315@gmail.com>
2022-12-17 23:17:09 +0900

    폴더 아키텍쳐 정리

---
+++ SocialNetworkBlogService/Core/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  SocialNetworkBlogService
+//
+//  Created by Hertz on 12/17/22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ SocialNetworkBlogService/SocialNetworkBlogServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkBlogServiceApp.swift
+//  SocialNetworkBlogService
+//
+//  Created by Hertz on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkBlogServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
